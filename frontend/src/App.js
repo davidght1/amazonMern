1bcca6aa59c4941f0ec9f59958a55afafc4219d1
David Agam <davidght1@gmail.com>
2023-01-28 12:02:34 +0200

    new file:   backend/data.js 	new file:   backend/package-lock.json 	new file:   backend/package.json 	new file:   backend/server.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/public/images/p1.jpg 	new file:   frontend/public/images/p2.jpg 	new file:   frontend/public/images/p3.jpg 	new file:   frontend/public/images/p4.jpg 	modified:   frontend/public/index.html 	modified:   frontend/src/App.js 	modified:   frontend/src/App.test.js 	new file:   frontend/src/components/LoadingBox.js 	new file:   frontend/src/components/MessageBox.js 	new file:   frontend/src/components/Product.js 	new file:   frontend/src/components/Rating.js 	new file:   frontend/src/data.js 	modified:   frontend/src/index.css 	modified:   frontend/src/index.js 	new file:   frontend/src/screens/HomeScreen.js 	new file:   frontend/src/screens/ProductScreen.js 	new file:   frontend/src/screens/Store.js     new file:   frontend/src/utils.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,52 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import Badge from "react-bootstrap/esm/Badge";
+import Nav from "react-bootstrap/esm/Nav";
+import { useContext } from "react";
+import { Store } from "./screens/Store";
+import { Link } from "react-router-dom";
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
